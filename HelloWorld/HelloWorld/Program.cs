0258c2cdd03cd565ea163215dd6c41918ed979bd
Neha Upadhyay <nehaupadhyay@Nehas-MacBook-Air.local>
2020-04-05 18:41:47 +0530

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseConfiguration(config)
+                .Build();
+            host.Run();
+        }
+    }
+}
